c8d09bd696e074bb2e6a0859570c3b52b67abab0
Wangzeyu <Wangzeyu3334111@foxmail.com>
2025-05-21 00:34:51 +0800

    feat: 更新模型URL为blue-cat模型，并调整相关配置

--- app/src/main/java/com/example/projectv2/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/projectv2/fragment/ProfileFragment.java
@@ -101,7 +101,7 @@ public class ProfileFragment extends Fragment implements LLamaAPI.ModelStateList
     private boolean isDownloading = false;
 //    private static final String MODEL_URL = "https://huggingface.co/qwqcoder/MiniCPM3-4B_Q4_K_M/resolve/main/MiniCPM3-4B-F16_Q4_k_m.gguf?download=true"; // 将这里替换为实际的模型URL
     private static final String MODEL_URL = "https://www.modelscope.cn/models/qwqcoder/blue-cat/resolve/master/model.gguf"; // 将这里替换为实际的模型URL
-    private static final String MODEL_FILENAME = "MiniCPM3-4B_Q4_k_m.gguf";
+    private static final String MODEL_FILENAME = "blue-cat.gguf";
 
     private static final int PICK_IMAGE_REQUEST = 1;
     private static final int PERMISSION_REQUEST = 2;
